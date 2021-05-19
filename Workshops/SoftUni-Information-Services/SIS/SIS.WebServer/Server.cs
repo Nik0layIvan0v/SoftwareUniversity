4ad7e54e558fdc07524bd9107b8582625c608d5f
Nikolay Ivanov <niki_8905@abv.bg>
2021-05-19 04:11:07 +0300

    Added SIS/SIS.WebServer project

---
+++ Workshops/SoftUni-Information-Services/SIS/SIS.WebServer/Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SIS.WebServer
+{
+    public class Server
+    {
+        public static void Main()
+        {
+            Console.WriteLine("I will be server");
+        }
+    }
+}
