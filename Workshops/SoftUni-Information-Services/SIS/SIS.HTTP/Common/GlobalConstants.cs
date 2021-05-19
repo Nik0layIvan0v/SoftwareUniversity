60d0ecb3797b162840c061d2bc67bcc502e48535
Nikolay Ivanov <niki_8905@abv.bg>
2021-05-19 04:18:32 +0300

    Introduced GlobalConstants class witch is used for shared constants.

---
+++ Workshops/SoftUni-Information-Services/SIS/SIS.HTTP/Common/GlobalConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace SIS.HTTP.Common
+{
+    /// <summary>
+    /// This class is used for shared constants.
+    /// </summary>
+    public static class GlobalConstants
+    {
+        public const string HttpOneProtocolFragment = "HTTP/1.1";
+
+        public const string HostHeaderKey = "Host";
+
+        public const string HttpNewLine = @"\r\n";
+    }
+}
