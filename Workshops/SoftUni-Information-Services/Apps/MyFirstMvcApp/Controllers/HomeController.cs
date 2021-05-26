3cd74287923d7912bc604c8a4c9a51dd299f13b9
Nikolay Ivanov <niki_8905@abv.bg>
2021-05-26 22:59:02 +0300

    Now getting the view logic is inside the base Controller. If you want to display view you need to follow these steps. 1. Inside your Apps/{name of app}/Controllers folder, create {Name}Controller and inherit the base Controller. 2. Put the HTML file in folder exactly named as {Name} of the derived controller but without the "Controller" part of the name. 3. When you want to display it on the browser you need to create Method inside the derived {Name}Controller named exactly as HTML file name. The method must receive HttpRequest type and return HttpResponse. 4. Register route/path by adding path and controller+method inside startup.cs => List<Route> routeTable 5. Inside the method just type return this.View(); and your ready.

--- Workshops/SoftUni-Information-Services/Apps/MyFirstMvcApp/Controllers/HomeController.cs
+++ Workshops/SoftUni-Information-Services/Apps/MyFirstMvcApp/Controllers/HomeController.cs
@@ -4,29 +4,16 @@ using SUS.MvcFramework;
 
 namespace MyFirstMvcApp.Controllers
 {
-    //Other stuffs like about page (no logic)
     public class HomeController : Controller
     {
         public HttpResponse Index(HttpRequest request)
         {
-            string responseHtml = "<h1>HomePage()</h1>";
-
-            byte[] responseBytes = Encoding.UTF8.GetBytes(responseHtml);
-
-            HttpResponse response = new HttpResponse("text/html", responseBytes);
-
-            return response;
+            return this.View();
         }
 
         public HttpResponse About(HttpRequest request)
         {
-            string responseHtml = "<h1>sever.AddRoute(\" /about\", (request) =></h1>";
-
-            byte[] responseBytes = Encoding.UTF8.GetBytes(responseHtml);
-
-            HttpResponse response = new HttpResponse("text/html", responseBytes);
-
-            return response;
+            return this.View();
         }
     }
 }
