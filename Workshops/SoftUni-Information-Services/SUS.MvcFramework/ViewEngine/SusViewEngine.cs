619f01c690a4730a3cd5ca8d46b24e51d6579fe4
Nikolay Ivanov <niki_8905@abv.bg>
2021-06-01 14:15:55 +0300

    Started to implement SusViewEngine and added IView Interface.

--- Workshops/SoftUni-Information-Services/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
+++ Workshops/SoftUni-Information-Services/SUS.MvcFramework/ViewEngine/SusViewEngine.cs
@@ -4,7 +4,30 @@
     {
         public string GetHtml(string templateCode, object viewModel)
         {
+            //TODO: Implement logic
+            
+            string csharpCode = GenerateCSharpFromTemplate(templateCode);
+
+            //3. Executable obj must have GenerateHtml method witch parameter is the view model.
+            IView executableObj = GenerateExecutableCode(csharpCode);
+
+            string readyHtml = executableObj.GenerateHtml(viewModel);
+
+            //4. Generated executable object call that method to generate actual ready HTML and return it through GetHtml()
+            return readyHtml;
+        }
+
+        //1. Get from Template Code only csharp part
+        public string GenerateCSharpFromTemplate(string template)
+        {
             throw new System.NotImplementedException();
         }
+
+        //2. Get only compile part from the csharp code;
+        private IView GenerateExecutableCode(string csharpCode)
+        {
+            throw new System.NotImplementedException();
+        }
+
     }
 }
