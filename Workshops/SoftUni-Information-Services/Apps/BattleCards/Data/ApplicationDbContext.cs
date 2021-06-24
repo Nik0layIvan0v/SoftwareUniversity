b5e0de2fadfd0494a5691817915f9b6a6cdb36b1
Nikolay Ivanov <niki_8905@abv.bg>
2021-06-24 03:56:17 +0300

    Started to implement new app called Battle cards.

---
+++ Workshops/SoftUni-Information-Services/Apps/BattleCards/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace BattleCards.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using static DatabaseConfiguration;
+
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Workshops/SoftUni-Information-Services/Apps/BattleCards/Startup.cs
@@ -0,0 +1,17 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using System.Collections.Generic;
+
+namespace BattleCards
+{
+    public class Startup : IMvcApplication
+    {
+        public void Configure(ICollection<Route> routeTable)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
